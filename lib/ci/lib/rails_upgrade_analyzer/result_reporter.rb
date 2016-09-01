d5320baf6c4e8f86c06c3f5a381630f3d05806a6
Kyle Stevens <kstevens715@gmail.com>
2016-09-01 15:12:53 -0400

    [NF-8840] Add deprecation warnings

--- lib/ci/lib/rails_upgrade_analyzer/result_reporter.rb
+++ lib/ci/lib/rails_upgrade_analyzer/result_reporter.rb
@@ -3,8 +3,17 @@ require "stringio"
 module RailsUpgradeAnalyzer
   class ResultReporter
 
-    def initialize(*job_results)
-      @job_results = job_results
+    def initialize(result1, result2)
+      @result1 = result1
+      @result2 = result2
+    end
+
+    def deprecation_report
+      @deprecation_report ||= build_deprecation_report
+    end
+
+    def deprecation_warnings_changed?
+      @deprecation_warnings_changed
     end
 
     def report
@@ -13,13 +22,42 @@ module RailsUpgradeAnalyzer
 
     private
 
-    attr_reader :job_results
+    attr_reader :result1, :result2
+
+    def all_deprecation_categories
+      (deprecations1.keys + deprecations2.keys).uniq.sort
+    end
+
+    def all_results
+      @all_results = [result1, result2]
+    end
+
+    def build_deprecation_report
+      io = StringIO.new
+      add_deprecation_report_header(io)
+
+      all_deprecation_categories.each do |category|
+        difference = deprecations2[category] - deprecations1[category]
+        if difference != 0
+          data = [category, deprecations1[category], deprecations2[category], difference]
+          io << data.join(" | ")
+          io << "\n"
+          @deprecation_warnings_changed = true
+        end
+      end
+
+      if !deprecation_warnings_changed?
+        io << "No deprecation differences found|"
+      end
+
+      io.string
+    end
 
     def build_report
       io = StringIO.new
-      add_header(io)
+      add_report_header(io)
 
-      job_results.each do |result|
+      all_results.each do |result|
         data = [result.description, result.tests, result.passed, result.failures, result.errors, result.passing_percent]
         io << data.join(" | ")
         io << "\n"
@@ -28,9 +66,24 @@ module RailsUpgradeAnalyzer
       io.string
     end
 
-    def add_header(io)
+    def deprecations1
+      result1.deprecations.default = 0
+      result1.deprecations
+    end
+
+    def deprecations2
+      result2.deprecations.default = 0
+      result2.deprecations
+    end
+
+    def add_report_header(io)
       io << "Branch | Tests | Passed | Failures | Errors | Passing %\n"
       io << ":----- | :---: | :----: | :------: | :----: | :-----:\n"
+    end
+
+    def add_deprecation_report_header(io)
+      io << "Deprecation | Result 1 | Result 2 | Difference\n"
+      io << ":-- | :--: | :--: | :--:\n"
     end
   end
 end
--- test/result_reporter_test.rb
+++ test/result_reporter_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TestResult < MiniTest::Unit::TestCase
 
-  def test_setting_attributes
+  def test_build_results
     result1 = RailsUpgradeAnalyzer::JobResult.new("1234.1", description: "Result 1", tests: 3, passed: 2, failures: 5, errors: 7)
     result2 = RailsUpgradeAnalyzer::JobResult.new("1234.2", description: "Result 2", tests: 11, passed: 13, failures: 17, errors: 19)
 
@@ -12,9 +12,40 @@ class TestResult < MiniTest::Unit::TestCase
     assert_equal ":----- | :---: | :----: | :------: | :----: | :-----:", line(reporter, 1)
     assert_equal "Result 1 | 3 | 2 | 5 | 7 | 66.67", line(reporter, 2)
     assert_equal "Result 2 | 11 | 13 | 17 | 19 | 118.18", line(reporter, 3)
+    assert !reporter.deprecation_warnings_changed?
+  end
+
+  def test_deprecation_warnings
+    deprecations1 = { "A warning" => 1, "Another warning" => 9, "No diff" => 5 }
+    deprecations2 = { "A warning" => 2, "No diff" => 5 }
+    result1 = RailsUpgradeAnalyzer::JobResult.new("1234.1", description: "Result 1", deprecations: deprecations1)
+    result2 = RailsUpgradeAnalyzer::JobResult.new("1234.2", description: "Result 2", deprecations: deprecations2)
+
+    reporter = RailsUpgradeAnalyzer::ResultReporter.new(result1, result2)
+
+    assert_equal "Deprecation | Result 1 | Result 2 | Difference", deprecation_line(reporter, 0)
+    assert_equal ":-- | :--: | :--: | :--:", deprecation_line(reporter, 1)
+    assert_equal "A warning | 1 | 2 | 1", deprecation_line(reporter, 2)
+    assert_equal "Another warning | 9 | 0 | -9", deprecation_line(reporter, 3)
+    assert_equal nil, deprecation_line(reporter, 4)
+    assert reporter.deprecation_warnings_changed?
+  end
+
+  def test_no_deprecations
+    result1 = RailsUpgradeAnalyzer::JobResult.new("1234.1", description: "Result 1")
+    result2 = RailsUpgradeAnalyzer::JobResult.new("1234.2", description: "Result 2")
+
+    reporter = RailsUpgradeAnalyzer::ResultReporter.new(result1, result2)
+
+    assert_equal "No deprecation differences found|", deprecation_line(reporter, 2)
+    assert !reporter.deprecation_warnings_changed?
   end
 
   def line(reporter, number)
     reporter.report.split("\n")[number]
+  end
+
+  def deprecation_line(reporter, number)
+    reporter.deprecation_report.split("\n")[number]
   end
 end
